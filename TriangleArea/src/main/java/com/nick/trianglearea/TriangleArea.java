a44f860b3ed27c97dada51b90ce0c9912098fb8f
peppapig450 <peppapig450@pm.me>
2025-08-25 21:04:40 -0700

    chore(trianglearea): scaffold TriangleArea for first project

---
+++ TriangleArea/src/main/java/com/nick/trianglearea/TriangleArea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nick.trianglearea;
+
+/**
+ *
+ * @author nick
+ */
+public class TriangleArea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
