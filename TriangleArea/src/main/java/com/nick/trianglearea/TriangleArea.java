3c9acffc0bf1974f0f493faa12cee08308804d6f
peppapig450 <peppapig450@pm.me>
2025-08-25 21:22:57 -0700

    feat(trianglearea): complete first assigment for TriangleArea

--- TriangleArea/src/main/java/com/nick/trianglearea/TriangleArea.java
+++ TriangleArea/src/main/java/com/nick/trianglearea/TriangleArea.java
@@ -4,6 +4,8 @@
 
 package com.nick.trianglearea;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nick
@@ -11,6 +13,17 @@ package com.nick.trianglearea;
 public class TriangleArea {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter the base of the triangle: ");
+        double triangleBase = userInput.nextDouble();
+
+        System.out.print("Enter the height of the triangle: ");
+        double triangleHeight = userInput.nextDouble();
+
+        double baseHeightProduct = triangleBase * triangleHeight;
+        double triangleArea = baseHeightProduct / 2.0;
+
+        System.out.printf("The area of the triangle is %.3f\n", triangleArea);
     }
 }
