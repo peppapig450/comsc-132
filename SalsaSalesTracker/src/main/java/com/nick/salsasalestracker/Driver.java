a67d6a52cf9ce6d31050c9f60e59d5e5dc78b413
peppapig450 <peppapig450@pm.me>
2025-09-11 19:06:57 -0700

    feat(salsasalestracker): make use of toString

--- SalsaSalesTracker/src/main/java/com/nick/salsasalestracker/Driver.java
+++ SalsaSalesTracker/src/main/java/com/nick/salsasalestracker/Driver.java
@@ -67,7 +67,7 @@ public class Driver {
 
             // "Sold" ends at column 25 when printed with %10s (S starts at 22).
             // To align numbers under the 'S' (column 22), use %7d (10 - 3 = 7).
-            System.out.printf("%-15s%7d\n", salsa.getName(), currentSales);
+            System.out.printf("%-15s%7d\n", salsa.toString(), currentSales);
 
             totalSales += currentSales;
 
