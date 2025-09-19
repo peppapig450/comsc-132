4707e8077d188981017c05fe9b3a43dab88fc91e
peppapig450 <peppapig450@pm.me>
2025-09-18 18:28:58 -0700

    chore: scaffold week 4 assignment

---
+++ Ships/src/main/java/com/nick/ships/Ships.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nick.ships;
+
+/**
+ *
+ * @author nick
+ */
+public class Ships {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
